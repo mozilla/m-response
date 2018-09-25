46054e02f5c2e6aa515d6e7c599f90bd8be73c72
Nathan <NathHorrigan@gmail.com>
2018-09-25 23:54:26 +0100

    responsive helpers

--- frontend/app/src/pages/index.js
+++ frontend/app/src/pages/index.js
@@ -11,7 +11,7 @@ export default class Index extends React.Component {
 						<span className="text-logo">M-Response</span>
 					</div>
 					<section className='info-container'>
-						<span className='tagline'>LIKE THE IDEA OF SAVING THE WORLD FROM YOUR COUCH?</span>
+						<p className='tagline-container'><span className='tagline'>LIKE THE IDEA OF SAVING THE WORLD FROM YOUR COUCH?</span></p>
 						<span className='info-desc'>This is a new and fun tool to make it fun and easy to respond as Mozilla on Google Play store reviews</span>
 					</section>
 					<button className='continue-button' onClick={() => alert('clicked')}>Let's Start</button>
