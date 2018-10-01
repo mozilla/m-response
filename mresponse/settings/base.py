ff3926f06e0a5b6d126ab8225198608b0527cb37
tm-kn <me@tmkn.org>
2018-10-01 15:32:55 +0100

    Add initial project template (#1)

---
+++ mresponse/settings/base.py
@@ -0,0 +1,474 @@
+"""
+Django settings for mresponse project.
+"""
+import os
+import sys
+
+import dj_database_url
+import raven
+from raven.exceptions import InvalidGitRepository
+
+env = os.environ.copy()
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
+
+
+# Switch off DEBUG mode explicitly in the base settings.
+# https://docs.djangoproject.com/en/stable/ref/settings/#debug
+DEBUG = False
+
+
+# Secret key is important to be kept secret. Never share it with anyone. Please
+# always set it in the environment variable and never check into the
+# repository.
+# In its default template Django generates a 50-characters long string using
+# the following function:
+# https://github.com/django/django/blob/fd8a7a5313f5e223212085b2e470e43c0047e066/django/core/management/utils.py#L76-L81
+# https://docs.djangoproject.com/en/stable/ref/settings/#allowed-hosts
+if 'SECRET_KEY' in env:
+    SECRET_KEY = env['SECRET_KEY']
+
+
+# Define what hosts an app can be accessed by.
+# It will return HTTP 400 Bad Request error if your host is not set using this
+# setting.
+# https://docs.djangoproject.com/en/stable/ref/settings/#allowed-hosts
+if 'ALLOWED_HOSTS' in env:
+    ALLOWED_HOSTS = env['ALLOWED_HOSTS'].split(',')
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    # This is an app that we use for the performance monitoring.
+    # You set configure it by setting the following environment variables:
+    #  * SCOUT_MONITOR="True"
+    #  * SCOUT_KEY="paste api key here"
+    #  * SCOUT_NAME="mresponse"
+    # https://intranet.torchbox.com/delivering-projects/tech/scoutapp/
+    # According to the official docs, it's important that Scout is listed
+    # first - http://help.apm.scoutapp.com/#django.
+    'scout_apm.django',
+
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+]
+
+
+# Middleware classes
+# https://docs.djangoproject.com/en/stable/ref/settings/#middleware
+# https://docs.djangoproject.com/en/stable/topics/http/middleware/
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+
+    # Whitenoise middleware is used to server static files (CSS, JS, etc.).
+    # According to the official documentation it should be listed underneath
+    # SecurityMiddleware.
+    # http://whitenoise.evans.io/en/stable/#quickstart-for-django-apps
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'mresponse.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(PROJECT_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'mresponse.wsgi.application'
+
+
+# Database
+# This setting will use DATABASE_URL environment variable.
+# https://docs.djangoproject.com/en/stable/ref/settings/#databases
+# https://github.com/kennethreitz/dj-database-url
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600)
+}
+
+
+# Server-side cache settings. Do not confuse with front-end cache.
+# https://docs.djangoproject.com/en/stable/topics/cache/
+# If the server has a Redis instance exposed via a URL string in the REDIS_URL
+# environment variable, prefer that. Otherwise use the database backend. We
+# usually use Redis in production and database backend on staging and dev. In
+# order to use database cache backend you need to run
+# "django-admin createcachetable" to create a table for the cache.
+
+# Do not use the same Redis instance for other things like Celery!
+if 'REDIS_URL' in env:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django_redis.cache.RedisCache',
+            'LOCATION': env['REDIS_URL'],
+        }
+    }
+else:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+            'LOCATION': 'database_cache',
+        }
+    }
+
+
+# Password validation
+# https://docs.djangoproject.com/en/stable/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/stable/topics/i18n/
+
+LANGUAGE_CODE = 'en-gb'
+
+TIME_ZONE = 'Europe/London'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/stable/howto/static-files/
+
+# We serve static files with Whitenoise (set in MIDDLEWARE). It also comes with
+# a custom backend for the static files storage. It makes files cacheable
+# (cache-control headers) for a long time and adds hashes to the file names,
+# e.g. main.css -> main.1jasdiu12.css.
+# The static files with this backend are generated when you run
+# "django-admin collectstatic".
+# http://whitenoise.evans.io/en/stable/#quickstart-for-django-apps
+# https://docs.djangoproject.com/en/stable/ref/settings/#staticfiles-storage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# This is where Django will look for static files outside the directories of
+# applications which are used by default.
+# https://docs.djangoproject.com/en/stable/ref/settings/#staticfiles-dirs
+STATICFILES_DIRS = [
+    # "static_compiled" is a folder used by the front-end tooling
+    # to output compiled static assets.
+    # os.path.join(PROJECT_DIR, 'static_compiled'),
+]
+
+
+# This is where Django will put files collected from application directories
+# and custom direcotires set in "STATICFILES_DIRS" when
+# using "django-admin collectstatic" command.
+# https://docs.djangoproject.com/en/stable/ref/settings/#static-root
+STATIC_ROOT = env.get('STATIC_DIR', os.path.join(BASE_DIR, 'static'))
+
+
+# This is the URL that will be used when serving static files, e.g.
+# https://llamasavers.com/static/
+# https://docs.djangoproject.com/en/stable/ref/settings/#static-url
+STATIC_URL = env.get('STATIC_URL', '/static/')
+
+
+# Where in the filesystem the media (user uploaded) content is stored.
+# MEDIA_ROOT is not used when S3 backend is set up.
+# Probably only relevant to the local development.
+# https://docs.djangoproject.com/en/stable/ref/settings/#media-root
+MEDIA_ROOT = env.get('MEDIA_DIR', os.path.join(BASE_DIR, 'media'))
+
+
+# The URL path that media files will be accessible at. This setting won't be
+# used if S3 backend is set up.
+# Probably only relevant to the local development.
+# https://docs.djangoproject.com/en/stable/ref/settings/#media-url
+MEDIA_URL = env.get('MEDIA_URL', '/media/')
+
+
+# AWS S3 buckets configuration
+# This is media files storage backend configuration. S3 is our preferred file
+# storage solution.
+# To enable this storage backend we use django-storages package...
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
+# ...that uses AWS' boto3 library.
+# https://boto3.amazonaws.com/v1/documentation/api/latest/index.html
+#
+# Three required environment variables are:
+#  * AWS_STORAGE_BUCKET_NAME
+#  * AWS_ACCESS_KEY_ID
+#  * AWS_SECRET_ACCESS_KEY
+# The last two are picked up by boto3:
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html#environment-variables
+if 'AWS_STORAGE_BUCKET_NAME' in env:
+    # Add django-storages to the installed apps
+    INSTALLED_APPS.append('storages')
+
+    # https://docs.djangoproject.com/en/stable/ref/settings/#default-file-storage
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_STORAGE_BUCKET_NAME = env['AWS_STORAGE_BUCKET_NAME']
+
+    # Disables signing of the S3 objects' URLs. When set to True it
+    # will append authorization querystring to each URL.
+    AWS_QUERYSTRING_AUTH = False
+
+    # Not having this setting may have consequences in losing files.
+    AWS_S3_FILE_OVERWRITE = False
+
+    # We generally use this setting in the production to put the S3 bucket
+    # behind a CDN using a custom domain, e.g. media.llamasavers.com.
+    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#cloudfront
+    if 'AWS_S3_CUSTOM_DOMAIN' in env:
+        AWS_S3_CUSTOM_DOMAIN = env['AWS_S3_CUSTOM_DOMAIN']
+
+    # This settings lets you force using http or https protocol when generating
+    # the URLs to the files. Set https as default.
+    # https://github.com/jschneier/django-storages/blob/10d1929de5e0318dbd63d715db4bebc9a42257b5/storages/backends/s3boto3.py#L217
+    AWS_S3_URL_PROTOCOL = env.get('AWS_S3_URL_PROTOCOL', 'https:')
+
+
+# Logging
+# This logging is configured to be used with Sentry and console logs. Console
+# logs are widely used by platforms offering Docker deployments, e.g. Heroku.
+# We use Sentry to only send error logs so we're notified about errors that are
+# not Python exceptions.
+# We do not use default mail or file handlers because they are of no use for
+# us.
+# https://docs.djangoproject.com/en/stable/topics/logging/
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        # Send logs with at least INFO level to the console.
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        # Send logs with level of at least ERROR to Sentry.
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s][%(process)d][%(levelname)s][%(name)s] %(message)s'
+        }
+    },
+    'loggers': {
+        'mresponse': {
+            'handlers': ['console', 'sentry'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console', 'sentry'],
+            'level': 'WARNING',
+            'propagate': False,
+        },
+        'django.security': {
+            'handlers': ['console', 'sentry'],
+            'level': 'WARNING',
+            'propagate': False,
+        },
+    },
+}
+
+
+# Email settings
+# We use SMTP to send emails. We typically use transactional email services
+# that let us use SMTP.
+# https://docs.djangoproject.com/en/2.1/topics/email/
+
+# https://docs.djangoproject.com/en/stable/ref/settings/#email-host
+if 'EMAIL_HOST' in env:
+    EMAIL_HOST = env['EMAIL_HOST']
+
+# https://docs.djangoproject.com/en/stable/ref/settings/#email-port
+if 'EMAIL_PORT' in env:
+    try:
+        EMAIL_PORT = int(env['EMAIL_PORT'])
+    except ValueError:
+        pass
+
+# https://docs.djangoproject.com/en/stable/ref/settings/#email-host-user
+if 'EMAIL_HOST_USER' in env:
+    EMAIL_HOST_USER = env['EMAIL_HOST_USER']
+
+# https://docs.djangoproject.com/en/stable/ref/settings/#email-host-password
+if 'EMAIL_HOST_PASSWORD' in env:
+    EMAIL_HOST_PASSWORD = env['EMAIL_HOST_PASSWORD']
+
+# https://docs.djangoproject.com/en/stable/ref/settings/#email-use-tls
+if env.get('EMAIL_USE_TLS', 'false').lower().strip() == 'true':
+    EMAIL_USE_TLS = True
+
+# https://docs.djangoproject.com/en/stable/ref/settings/#email-use-ssl
+if env.get('EMAIL_USE_SSL', 'false').lower().strip() == 'true':
+    EMAIL_USE_SSL = True
+
+# https://docs.djangoproject.com/en/stable/ref/settings/#email-subject-prefix
+if 'EMAIL_SUBJECT_PREFIX' in env:
+    EMAIL_SUBJECT_PREFIX = env['EMAIL_SUBJECT_PREFIX']
+
+# SERVER_EMAIL is used to send emails to administrators.
+# https://docs.djangoproject.com/en/stable/ref/settings/#server-email
+# DEFAULT_FROM_EMAIL is used as a default for any mail send from the website to
+# the users.
+# https://docs.djangoproject.com/en/stable/ref/settings/#default-from-email
+if 'SERVER_EMAIL' in env:
+    SERVER_EMAIL = DEFAULT_FROM_EMAIL = env['SERVER_EMAIL']
+
+
+# Raven (Sentry) configuration.
+# See instructions on the intranet:
+# https://intranet.torchbox.com/delivering-projects/tech/starting-new-project/#sentry
+
+if 'SENTRY_DSN' in env:
+    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+
+    RAVEN_CONFIG = {
+        'dsn': env['SENTRY_DSN'],
+        'tags': {},
+    }
+
+    # Specifying the programming language as a tag can be useful when
+    # e.g. JavaScript error logging is enabled within the same project,
+    # so that errors can be filtered by the programming language too.
+    # The 'lang' tag is just an arbitrarily chosen one; any other tags can be used as well.
+    # It has to be overridden in JavaScript: Raven.setTagsContext({lang: 'javascript'});
+    RAVEN_CONFIG['tags']['lang'] = 'python'
+
+    # Prevent logging errors from the django shell.
+    # Errors from other management commands will be still logged.
+    if len(sys.argv) > 1 and sys.argv[1] in ['shell', 'shell_plus']:
+        RAVEN_CONFIG['ignore_exceptions'] = ['*']
+
+    # There's a chooser to toggle between environments at the top right corner on sentry.io
+    # Values are typically 'staging' or 'production' but can be set to anything else if needed.
+    # dokku config:set mresponse SENTRY_ENVIRONMENT=staging
+    # heroku config:set SENTRY_ENVIRONMENT=production
+    if 'SENTRY_ENVIRONMENT' in env:
+        RAVEN_CONFIG['environment'] = env['SENTRY_ENVIRONMENT']
+
+    # We first assume that the Git repository is present and we can detect the
+    # commit hash from it.
+    try:
+        RAVEN_CONFIG['release'] = raven.fetch_git_sha(BASE_DIR)
+    except InvalidGitRepository:
+        try:
+            # But if it's not, we assume that the commit hash is available in
+            # the GIT_REV environment variable. It's a default environment
+            # variable used on Dokku:
+            # http://dokku.viewdocs.io/dokku/deployment/methods/git/#configuring-the-git_rev-environment-variable
+            RAVEN_CONFIG['release'] = env['GIT_REV']
+        except KeyError:
+            # If there's no commit hash, we do not set a specific release.
+            pass
+
+
+# Security configuration
+# This configuration is required to achieve good security rating.
+# You can test it using https://securityheaders.com/
+# https://docs.djangoproject.com/en/stable/ref/middleware/#module-django.middleware.security
+
+# Force HTTPS redirect
+# https://docs.djangoproject.com/en/stable/ref/settings/#secure-ssl-redirect
+if env.get('SECURE_SSL_REDIRECT', 'true').strip().lower() == 'true':
+    SECURE_SSL_REDIRECT = True
+
+
+# This will allow the cache to swallow the fact that the website is behind TLS
+# and inform the Django using "X-Forwarded-Proto" HTTP header.
+# https://docs.djangoproject.com/en/stable/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+# This is a setting setting HSTS header. This will enforce the visitors to use
+# HTTPS for an amount of time specified in the header. Please make sure you
+# consult with sysadmin before setting this.
+# https://docs.djangoproject.com/en/stable/ref/settings/#secure-hsts-seconds
+if 'SECURE_HSTS_SECONDS' in env:
+    SECURE_HSTS_SECONDS = int(env['SECURE_HSTS_SECONDS'])
+
+
+# https://docs.djangoproject.com/en/stable/ref/settings/#secure-browser-xss-filter
+if env.get('SECURE_BROWSER_XSS_FILTER', 'true').lower().strip() == 'true':
+    SECURE_BROWSER_XSS_FILTER = True
+
+
+# https://docs.djangoproject.com/en/stable/ref/settings/#secure-content-type-nosniff
+if env.get('SECURE_CONTENT_TYPE_NOSNIFF', 'true').lower().strip() == 'true':
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Basic authentication settings
+# These are settings to configure the third-party library:
+# https://gitlab.com/tmkn/django-basic-auth-ip-whitelist
+if env.get('BASIC_AUTH_ENABLED', 'false').lower().strip() == 'true':
+    # Insert basic auth as a first middleware to be checked first, before
+    # anything else.
+    MIDDLEWARE.insert(0, 'baipw.middleware.BasicAuthIPWhitelistMiddleware')
+
+    # This is the credentials users will have to use to access the site.
+    BASIC_AUTH_LOGIN = env.get('BASIC_AUTH_LOGIN', 'mresponse')
+    BASIC_AUTH_PASSWORD = env.get('BASIC_AUTH_PASSWORD', '{{ cookiecutter.http_auth_password }}')
+
+    # This is the list of network IP addresses that are allowed in without
+    # basic authentication check.
+    BASIC_AUTH_WHITELISTED_IP_NETWORKS = [
+        # Torchbox networks.
+        # https://projects.torchbox.com/projects/sysadmin/notebook/IP%20addresses%20to%20whitelist
+        '78.32.251.192/28',
+        '89.197.53.244/30',
+        '193.227.244.0/23',
+        '2001:41c8:103::/48',
+    ]
+
+    # This is the list of hosts that website can be accessed without basic auth
+    # check. This may be useful to e.g. white-list "llamasavers.com" but not
+    # "llamasavers.production.torchbox.com".
+    if 'BASIC_AUTH_WHITELISTED_HTTP_HOSTS' in env:
+        BASIC_AUTH_WHITELISTED_HTTP_HOSTS = (
+            env['BASIC_AUTH_WHITELISTED_HTTP_HOSTS'].split(',')
+        )
---
+++ mresponse/settings/dev.py
@@ -0,0 +1,35 @@
+from .base import *  # noqa
+
+# Debugging to be enabled locally only
+DEBUG = True
+
+
+# This key to be used locally only.
+SECRET_KEY = 'CHANGEME!!!'
+
+
+# Allow all the hosts locally only.
+ALLOWED_HOSTS = ['*']
+
+
+# Allow requests from the local IPs to see more debug information.
+INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
+
+
+# Display sent emails in the console while developing locally.
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# Disable password validators when developing locally.
+AUTH_PASSWORD_VALIDATORS = []
+
+# Disable forcing HTTPS locally since development server supports HTTP only.
+SECURE_SSL_REDIRECT = False
+
+
+# Import settings from local.py file if it exists. Please use it to keep
+# settings that are not meant to be checked into Git and never check it in.
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
