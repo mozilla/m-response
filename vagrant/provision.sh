37a108325c1cab2d9b82d92ad6ceecb9a555b802
Tomasz Knapik <me@tmkn.org>
2018-10-03 13:30:29 +0100

    Update CI with the front-end tooling

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -74,3 +74,8 @@ EOF
 curl -sSL https://deb.nodesource.com/setup_8.x | bash -
 apt-get install -y nodejs
 
+# Install Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update -y
+apt-get install -y yarn
