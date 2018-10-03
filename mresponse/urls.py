7889ae11f0354c5f49e514bce0f4323da3b91b7c
Tomasz Knapik <me@tmkn.org>
2018-10-03 13:34:32 +0100

    Fix urls.py

--- mresponse/urls.py
+++ mresponse/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
