50ddf7e480598e22b3cc89f9172ad6b30c761b4f
Tomasz Knapik <me@tmkn.org>
2018-10-04 17:18:56 +0100

    Fix isort

--- mresponse/urls.py
+++ mresponse/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
