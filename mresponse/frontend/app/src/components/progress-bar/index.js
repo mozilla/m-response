6a4a1b749da528ca7d864fd43b93c796b108ef96
Nathan <NathHorrigan@gmail.com>
2018-10-07 20:25:28 +0100

    Profile Page UI Update

--- mresponse/frontend/app/src/components/progress-bar/index.js
+++ mresponse/frontend/app/src/components/progress-bar/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './progress-bar.scss'
 
 export default ({ width = 0, className, value, maxValue }) => {
+<<<<<<< HEAD
   if (value != null && maxValue != null) {
     width = (value / maxValue) * 100
   }
@@ -13,3 +14,17 @@ export default ({ width = 0, className, value, maxValue }) => {
     </div>
   )
 }
+=======
+    
+    if (value != null && maxValue != null) {
+        width = (value / maxValue) * 100
+    }
+
+    return (
+        <div className={`progress-bar ${className}`}>
+            <div className='progress-bar-inner' style={{ width: `${width}%` }} />
+            <span className='progress-bar-label'>{value} / {maxValue}</span>
+        </div>
+    )
+}
+>>>>>>> Profile Page UI Update
--- mresponse/frontend/app/src/components/response-card/index.js
+++ mresponse/frontend/app/src/components/response-card/index.js
@@ -4,6 +4,7 @@ import './response-card.scss'
 export default class ResponseCard extends React.Component {
     state = { expanded: false }
 
+<<<<<<< HEAD
     render () {
       const { className, response, date, productImage, productName } = this.props
       const comment = (this.state.expanded && response)
@@ -34,3 +35,36 @@ export default class ResponseCard extends React.Component {
 
     toggleExpansion = () => this.setState({ expanded: !this.state.expanded })
 }
+=======
+    render() {
+        const { className, response, date, productImage, productName } = this.props
+        const comment = (this.state.expanded && response)
+            ? response
+            : response.substring(0, 250) + '...'
+        return (
+            <div className={`response-card ${className}`}>
+                <div className='response-card-header'>
+                    <img
+                        className='response-card-header-image'
+                        src={productImage} alt='' />
+                    <span className='response-card-header-title'>{productName}</span>
+                </div>
+                <div className={`response-card-comment ${this.state.expanded ? 'response-card-comment--expanded' : ''}`}>
+                    <p className='response-card-comment-text'>{comment}</p>
+                </div>
+                <div className='response-card-footer'>
+                    <span className='response-card-footer-date'>{date}</span>
+                    <span 
+                        className='response-card-footer-expand'
+                        onClick={this.toggleExpansion}>
+                            {this.state.expanded ? 'Read Less' : 'Read More'}
+                        </span>
+                </div>
+            </div>
+        )
+    }
+
+    toggleExpansion = () => this.setState({ expanded: !this.state.expanded })
+
+}
+>>>>>>> Profile Page UI Update
