475682b55fa50be6801ad884f9f18a5bbf8135b2
Nathan <NathHorrigan@gmail.com>
2018-10-07 05:48:43 +0100

    welcome page update

--- mresponse/frontend/app/src/pages/welcome/welcome.js
+++ mresponse/frontend/app/src/pages/welcome/welcome.js
@@ -7,7 +7,7 @@ import Button from '@components/buttons'
 import './welcome.scss'
 
 export default class WelcomePage extends React.Component {
-  render () {
+  render() {
     return (
       <div className="welcome-page">
         <img
@@ -17,14 +17,14 @@ export default class WelcomePage extends React.Component {
         />
         <div className="page-content">
           <div className="brand-container">
-            <Logo className="logo-foo" />
-            <span className="text-logo">M-Response</span>
+            <img
+              className="welcome-image"
+              src="static/media/welcome-image.png"
+              alt=""
+            />
           </div>
           <section className="info-container">
-            <HighlightedText
-              textClassName="tagline"
-              text="LIKE THE IDEA OF SAVING THE WORLD FROM YOUR COUCH?"
-            />
+            <span className="info-tagline">Saving The World From Your Couch</span>
             <span className="info-desc">
               This is a new and fun tool to make it fun and easy to respond as
               Mozilla on Google Play store reviews
