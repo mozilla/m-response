fa8e8aa00f7149a23e95799c67e6c47b117a137c
Tomasz Knapik <tomasz.knapik@torchbox.com>
2018-10-08 15:15:14 +0100

    Display human readable version in the admin view

--- mresponse/reviews/admin.py
+++ mresponse/reviews/admin.py
@@ -3,5 +3,5 @@ from mresponse.reviews import models as reviews_models
 
 
 @admin.register(reviews_models.Review)
-class AuthorAdmin(admin.ModelAdmin):
-    pass
+class ReviewAdmin(admin.ModelAdmin):
+    readonly_fields = ('android_version',)
