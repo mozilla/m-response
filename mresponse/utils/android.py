77ea8cad3b611c82ab8fb45edcf139730a13f772
Tomasz Knapik <tomasz.knapik@torchbox.com>
2018-10-08 16:43:23 +0100

    Add another fallback option for lack of Android version

--- mresponse/utils/android.py
+++ mresponse/utils/android.py
@@ -38,5 +38,9 @@ def get_android_version(android_sdk_number):
 def get_human_readable_android_version(android_sdk_number):
     version = get_android_version(android_sdk_number)
     if version is None:
+        if android_sdk_number:
+            return _('SDK %(sdk_version)d') % {
+                'sdk_version': android_sdk_number,
+            }
         return _('Unknown')
     return '.'.join(str(v) for v in version)
