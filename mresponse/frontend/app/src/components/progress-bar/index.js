4f3601747afcd4a08a3c598ff8f839d1de8db2db
Nathan <NathHorrigan@gmail.com>
2018-10-08 18:27:46 +0100

    linting

--- mresponse/frontend/app/src/components/progress-bar/index.js
+++ mresponse/frontend/app/src/components/progress-bar/index.js
@@ -2,11 +2,9 @@ import React from 'react'
 import './progress-bar.scss'
 
 export default ({ width = 0, className, value, maxValue }) => {
-<<<<<<< HEAD
   if (value != null && maxValue != null) {
     width = (value / maxValue) * 100
   }
-
   return (
     <div className={`progress-bar ${className}`}>
       <div className='progress-bar-inner' style={{ width: `${width}%` }} />
@@ -14,17 +12,3 @@ export default ({ width = 0, className, value, maxValue }) => {
     </div>
   )
 }
-=======
-    
-    if (value != null && maxValue != null) {
-        width = (value / maxValue) * 100
-    }
-
-    return (
-        <div className={`progress-bar ${className}`}>
-            <div className='progress-bar-inner' style={{ width: `${width}%` }} />
-            <span className='progress-bar-label'>{value} / {maxValue}</span>
-        </div>
-    )
-}
->>>>>>> Profile Page UI Update
