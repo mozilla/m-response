c1240af860634ed4ad441d58b04b7e14dd1e489b
Tomasz Knapik <hi@tmkn.org>
2018-10-12 12:57:48 +0100

    Fix APPEND_SLASH catch all url for the front-end (#77)

--- mresponse/frontend/urls.py
+++ mresponse/frontend/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
         ),
         name='manifest.json',
     ),
-    re_path('', TemplateView.as_view(template_name="index.html"))
+    re_path(r'.*/$', TemplateView.as_view(template_name="index.html"))
 ]
