cfc7f397598c2e032d3503df746e48f8f6e55dd7
Tomasz Knapik <tomasz.knapik@torchbox.com>
2018-10-15 13:12:49 +0100

    Add karma and response/moderation count to user

---
+++ mresponse/users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin, auth
+
+from mresponse.users import models as users_models
+
+
+admin.site.unregister(auth.get_user_model())
+
+
+class UserProfileInline(admin.StackedInline):
+    model = users_models.UserProfile
+
+
+@admin.register(auth.get_user_model())
+class UserAdmin(auth.admin.UserAdmin):
+    inlines = (UserProfileInline,)
---
+++ mresponse/users/apps.py
@@ -0,0 +1,9 @@
+from django import apps
+
+
+class UsersConfig(apps.AppConfig):
+    name = 'mresponse.users'
+
+    def ready(self):
+        from mresponse.users.signal_handlers import register_signal_handlers
+        register_signal_handlers()
