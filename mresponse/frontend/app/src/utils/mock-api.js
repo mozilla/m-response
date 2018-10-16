e22e51229c292250f6044a71507a02ca954b284f
Nathan <NathHorrigan@gmail.com>
2018-10-16 11:16:17 +0100

    mock api match real endpoint data

--- mresponse/frontend/app/src/utils/mock-api.js
+++ mresponse/frontend/app/src/utils/mock-api.js
@@ -4,7 +4,7 @@ import { staticAsset } from '@utils/urls'
 
 export default class Api {
   
-  getConfig () {
+  getConfig() {
     return {
       'languages': [
         {
@@ -29,7 +29,7 @@ export default class Api {
     }
   }
 
-  getHomeConfig () {
+  getHomeConfig() {
     return {
       'respond_queue': 31240,
       'moderate_queue': 1800
@@ -46,22 +46,43 @@ export default class Api {
     }
   }
 
-  getReview () {
+  getReview() {
+    const res =  {
+      "id": faker.random.number(100000),
+      "android_sdk_version": 22,
+      "android_version": "7.1",
+      "author_name": faker.name.findName(),
+      "application": {
+        "name": "Firefox",
+        "package": "org.mozilla.firefox"
+      },
+      "application_version": {
+        "name": "1.0.1",
+        "code": 68
+      },
+      "review_text":  faker.lorem.paragraphs(1),
+      "review_rating": faker.random.number(3),
+      "last_modified": faker.date.past(1),
+      "assignment_expires_at": Date.now() + 3600000,
+      "response_url": "https://mresponse-staging.herokuapp.com/api/respond/1/",
+      "skip_url": "https://mresponse-staging.herokuapp.com/api/review/skip/1/"
+    }
+
     return {
-      id: faker.random.number(100000),
-      author: faker.name.findName(),
-      rating: faker.random.number(3),
-      text: faker.lorem.paragraphs(1),
+      id: res['id'],
+      author: res['author_name'],
+      rating: res['rating'],
+      text: res['review_text'],
       product: {
-        name: 'Firefox 59.0.2',
+        name: res['application']['name'] + ' ' + res['application_version']['name'],
         image: staticAsset('media/firefox.png')
       },
-      androidVersion: 'Android 7.07',
-      dateSubmitted: faker.date.past(1)
+      androidVersion: 'Android ' + res['android_version'],
+      dateSubmitted: res['last_modified']
     }
   }
 
-  submitResponse (reviewId, response) {
+  submitResponse(reviewId, response) {
     return new Promise((resolve, reject) => {
       if (reviewId && response) {
         resolve({ detail: 'Thank you for your effort and so making Mozilla better for all of us!' })
@@ -71,7 +92,7 @@ export default class Api {
     })
   }
 
-  skipReview (reviewId) {
+  skipReview(reviewId) {
     return new Promise((resolve, reject) => {
       if (reviewId) {
         resolve({ detail: 'Review Skipped!' })
