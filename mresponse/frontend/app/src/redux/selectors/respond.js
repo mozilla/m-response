87cc2552e8c93f3f8bc11b65dab5cfb784118adc
Nathan <NathHorrigan@gmail.com>
2018-10-16 11:53:37 +0100

    review selectors

---
+++ mresponse/frontend/app/src/redux/selectors/respond.js
@@ -0,0 +1,16 @@
+const emptyReview = {
+  id: '',
+  author: '',
+  rating: 0,
+  text: '',
+  product: {
+    name: '',
+    image: ''
+  },
+  androidVersion: '',
+  dateSubmitted: 0,
+  assignmentExpiration: 0
+}
+
+export const getCurrentReview = state => state.respond.currentReview || emptyReview
+export const getNextReview = state => state.respond.nextReview || emptyReview
