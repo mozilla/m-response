32b8bb710239f92eabb5826c5d2474f2ab2bbf32
Karl Hobley <karl@kaed.uk>
2018-10-18 15:18:48 +0100

    Fixes to fetch_reviews command

--- mresponse/reviews/management/commands/fetch_reviews.py
+++ mresponse/reviews/management/commands/fetch_reviews.py
@@ -11,10 +11,10 @@ from mresponse.reviews.models import Review
 class Command(BaseCommand):
     help = "Fetches all available reviews from Google playstore"
 
-    def get_reviews(self, package_name):
+    def get_reviews(self, application):
         service = _connect(settings.PLAY_ACCOUNT, settings.PLAY_CREDENTIALS_PATH)
         reviews_service = service.reviews()
-        results = reviews_service.list(packageName=package_name).execute()
+        results = reviews_service.list(packageName=application.package).execute()
 
         while results:
             for review in results['reviews']:
@@ -22,28 +22,28 @@ class Command(BaseCommand):
                     comment = review['comments'][0]['userComment']
                     kwargs = {
                         'play_store_review_id': review['reviewId'],
-                        'android_sdk_version': comment['androidOsVersion'],
+                        'android_sdk_version': comment.get('androidOsVersion'),
                         'author_name': review['authorName'],
                         'review_text': comment['text'],
                         'review_rating': comment['starRating'],
                         'last_modified': datetime.fromtimestamp(
-                            int(comment['last_modified']['seconds'])
+                            int(comment['lastModified']['seconds'])
                         )
                     }
 
                     # TODO: Add application version
                     obj = Review(**kwargs)
-                    obj.application = Application.objects.get(name=package_name)
+                    obj.application = application
                     obj.save()
 
             if results['tokenPagination']['nextPageToken']:
                 nextPageToken = results['tokenPagination']['nextPageToken']
                 results = reviews_service.list(
-                    packageName=package_name, token=nextPageToken
+                    packageName=application.package, token=nextPageToken
                 ).execute()
             else:
                 break
 
     def handle(self, *args, **kwargs):
         for application in Application.objects.all():
-            self.get_reviews(application.name)
+            self.get_reviews(application)
