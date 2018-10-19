6aa1a96aefe1560ba5f8f9a88de71ed5eae76a3c
Karl Hobley <karl@kaed.uk>
2018-10-19 09:44:05 +0100

    Add review_language field

--- mresponse/reviews/models.py
+++ mresponse/reviews/models.py
@@ -31,6 +31,7 @@ class Review(models.Model):
         related_name='+'
     )
     review_text = models.TextField()
+    review_language = models.CharField(max_length=5)
     review_rating = models.SmallIntegerField(choices=REVIEW_RATING_CHOICES)
     last_modified = models.DateTimeField()
     assigned_to = models.OneToOneField(
