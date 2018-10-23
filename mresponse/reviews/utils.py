48eaf4317c204291aff87f88c60c26b276e83960
John Giannelos <johngiannelos@gmail.com>
2018-10-23 23:18:42 +0200

    Replace direct api calls with proxy service.

--- mresponse/reviews/utils.py
+++ mresponse/reviews/utils.py
@@ -1,17 +1,14 @@
 from django.conf import settings
 
-from mozapkpublisher.common.googleplay import _connect
+import requests
 
 
 def reply_to_review(application, review_id, text):
     """Reply to playstore reviews based on application/review_id"""
 
-    service = _connect(settings.PLAY_ACCOUNT, settings.PLAY_CREDENTIALS_PATH)
-    service = service.reviews()
-    reply = service.reply(
-        packageName=application.package,
-        reviewId=review_id,
-        body={"replyText": text}
-    ).execute()
+    params = {'packageName': application.package, 'reviewId': review_id, 'text': text}
+    headers = {'x-api-key': settings.REVIEWS_API_KEY}
+    response = requests.post(settings.REVIEWS_API_URL, params=params, headers=headers)
+    response.raise_for_status()
 
-    return reply
+    return response.json()
