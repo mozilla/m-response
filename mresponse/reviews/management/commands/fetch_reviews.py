ec3c28d0aa07a1db4c9be0779d44d8676547a4f8
Tomasz Knapik <hi@tmkn.org>
2018-10-25 15:06:44 +0100

    Sleep for a minute between each API fetch (#177)
    * Sleep for a minute between each fetch

--- mresponse/reviews/management/commands/fetch_reviews.py
+++ mresponse/reviews/management/commands/fetch_reviews.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from datetime import datetime, timedelta
 
 from django.conf import settings
@@ -18,7 +19,6 @@ logger = logging.getLogger('mresponse.reviews.fetch_reviews')
 class Command(BaseCommand):
     help = "Fetches all available reviews from Google playstore"
 
-    @transaction.atomic
     def get_reviews(self, application):
         logger.info("Fetching new reviews for application: %s", application)
 
@@ -32,57 +32,57 @@ class Command(BaseCommand):
 
         while results:
             time_of_last_review = None
+            with transaction.atomic():
+                for review in results['reviews']:
+                    # Reviews are returned in reverse-chronological order so this import
+                    # can stop importing when it reaches a target date/time. This target is
+                    # either the date/time of the most review we've imported before or one
+                    # week ago, which ever is closer to the current time.
 
-            for review in results['reviews']:
-                # Reviews are returned in reverse-chronological order so this import
-                # can stop importing when it reaches a target date/time. This target is
-                # either the date/time of the most review we've imported before or one
-                # week ago, which ever is closer to the current time.
-
-                # Stop when we reach a review that's already imported
-                if Review.objects.filter(play_store_review_id=review['reviewId']).exists():
-                    return
-
-                if len(review['comments']) == 1:
-                    comment = review['comments'][0]['userComment']
-                    kwargs = {
-                        'play_store_review_id': review['reviewId'],
-                        'android_sdk_version': comment.get('androidOsVersion'),
-                        'author_name': review.get('authorName', ''),
-                        'review_text': comment['text'],
-                        'review_language': comment['reviewerLanguage'],
-                        'review_rating': comment['starRating'],
-                        'last_modified': timezone.make_aware(datetime.fromtimestamp(
-                            int(comment['lastModified']['seconds'])
-                        ), pytz.UTC)
-                    }
-
-                    # Application version
-                    if 'appVersionCode' in comment:
-                        version_code = comment['appVersionCode']
-
-                        if version_code not in versions_cache:
-                            version, created = ApplicationVersion.objects.get_or_create(
-                                application=application,
-                                code=version_code,
-                                defaults={
-                                    'name': comment['appVersionName']
-                                }
-                            )
-
-                            versions_cache[version_code] = version
-
-                        kwargs['application_version'] = versions_cache[version_code]
-
-                    # Stop when we reach a review that's older than one week
-                    if kwargs['last_modified'] < timezone.now() - timedelta(days=7):
+                    # Stop when we reach a review that's already imported
+                    if Review.objects.filter(play_store_review_id=review['reviewId']).exists():
                         return
 
-                    obj = Review(**kwargs)
-                    obj.application = application
-                    obj.save()
+                    if len(review['comments']) == 1:
+                        comment = review['comments'][0]['userComment']
+                        kwargs = {
+                            'play_store_review_id': review['reviewId'],
+                            'android_sdk_version': comment.get('androidOsVersion'),
+                            'author_name': review.get('authorName', ''),
+                            'review_text': comment['text'],
+                            'review_language': comment['reviewerLanguage'],
+                            'review_rating': comment['starRating'],
+                            'last_modified': timezone.make_aware(datetime.fromtimestamp(
+                                int(comment['lastModified']['seconds'])
+                            ), pytz.UTC)
+                        }
 
-                    time_of_last_review = kwargs['last_modified']
+                        # Application version
+                        if 'appVersionCode' in comment:
+                            version_code = comment['appVersionCode']
+
+                            if version_code not in versions_cache:
+                                version, created = ApplicationVersion.objects.get_or_create(
+                                    application=application,
+                                    code=version_code,
+                                    defaults={
+                                        'name': comment['appVersionName']
+                                    }
+                                )
+
+                                versions_cache[version_code] = version
+
+                            kwargs['application_version'] = versions_cache[version_code]
+
+                        # Stop when we reach a review that's older than one week
+                        if kwargs['last_modified'] < timezone.now() - timedelta(days=7):
+                            return
+
+                        obj = Review(**kwargs)
+                        obj.application = application
+                        obj.save()
+
+                        time_of_last_review = kwargs['last_modified']
 
             if len(results['reviews']) > 0:
                 logger.info("Fetched %d reviews (%s)", len(results['reviews']), time_of_last_review)
@@ -90,6 +90,10 @@ class Command(BaseCommand):
                 logger.info("Fetched 0 reviews")
 
             if results['tokenPagination']['nextPageToken']:
+                # Sleep for a minute
+                logger.info("Sleep for a minute before next request")
+                time.sleep(60)
+
                 nextPageToken = results['tokenPagination']['nextPageToken']
                 params = {'packageName': application.package, 'token': nextPageToken}
                 response = requests.get(settings.REVIEWS_API_URL, params=params, headers=headers)
