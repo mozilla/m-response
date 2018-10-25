83c79ecb945a3f21f6c01cc7653941682043a71c
Tomasz Knapik <tomasz.knapik@torchbox.com>
2018-10-25 09:44:59 +0100

    Add URLs to app config endpoint

--- mresponse/api/views.py
+++ mresponse/api/views.py
@@ -20,94 +20,89 @@ def api_root(request, format=None):
             format=format,
         )
         response_dict['review'] = reverse.reverse(
-            'get_review',
-            request=request,
-            format=format
-        )
+            'get_review', request=request, format=format)
         response_dict['response'] = reverse.reverse(
-            'get_response',
-            request=request,
-            format=format
-        )
+            'get_response', request=request, format=format)
     response_dict['config'] = reverse.reverse(
-        'config',
-        request=request,
-        format=format
-    )
+        'config', request=request, format=format)
     return response.Response(response_dict)
 
 
 class Config(views.APIView):
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (permissions.AllowAny, )
 
     def get(self, request, format=None):
         response_dict = collections.OrderedDict()
         response_dict['languages'] = self.get_languages()
-        response_dict['response_guide_book_url'] = 'http://dgg.gg'
-        response_dict['feedback_url'] = (
-            'https://github.com/torchbox/m-response/issues/new'
+        response_dict['response_guide_book_url'] = (
+            'https://docs.google.com/document/d/1dtTS0hAIYyf9JWoqPc_S2tg2XmdptVYI06koGCZwiXc/'
         )
+        response_dict['feedback_url'] = (
+            'https://github.com/torchbox/m-response/issues/new')
         response_dict['about_url'] = 'https://www.mozilla.org/en-US/about/'
-        response_dict['legal_url'] = 'http://dgg.gg'
-        response_dict['privacy_url'] = 'http://dgg.gg'
-        response_dict['cookies_url'] = 'http://dgg.gg'
+        response_dict['legal_url'] = 'https://www.mozilla.org/about/legal/'
+        response_dict['privacy_url'] = 'https://www.mozilla.org/privacy/'
+        response_dict[
+            'cookies_url'] = 'https://www.mozilla.org/privacy/websites/#cookies'
         return response.Response(response_dict)
 
     def get_languages(self):
-        languages = [pycountry.languages.get(alpha_2=lang) for lang in [
-            'af',
-            'ar',
-            'az',
-            'bn',
-            'bg',
-            'my',
-            'ca',
-            'zh',
-            'hr',
-            'cs',
-            'da',
-            'nl',
-            'en',
-            'et',
-            'fi',
-            'fr',
-            'ka',
-            'de',
-            'el',
-            'he',
-            'hi',
-            'hu',
-            'is',
-            'id',
-            'it',
-            'ja',
-            'km',
-            'ko',
-            'lo',
-            'lv',
-            'lt',
-            'mk',
-            'ms',
-            'ml',
-            'mr',
-            'no',
-            'fa',
-            'pl',
-            'pt',
-            'ro',
-            'ru',
-            'sr',
-            'sk',
-            'sl',
-            'es',
-            'sv',
-            'tl',
-            'te',
-            'th',
-            'tr',
-            'uk',
-            'vi',
-        ]]
+        languages = [
+            pycountry.languages.get(alpha_2=lang) for lang in [
+                'af',
+                'ar',
+                'az',
+                'bn',
+                'bg',
+                'my',
+                'ca',
+                'zh',
+                'hr',
+                'cs',
+                'da',
+                'nl',
+                'en',
+                'et',
+                'fi',
+                'fr',
+                'ka',
+                'de',
+                'el',
+                'he',
+                'hi',
+                'hu',
+                'is',
+                'id',
+                'it',
+                'ja',
+                'km',
+                'ko',
+                'lo',
+                'lv',
+                'lt',
+                'mk',
+                'ms',
+                'ml',
+                'mr',
+                'no',
+                'fa',
+                'pl',
+                'pt',
+                'ro',
+                'ru',
+                'sr',
+                'sk',
+                'sl',
+                'es',
+                'sv',
+                'tl',
+                'te',
+                'th',
+                'tr',
+                'uk',
+                'vi',
+            ]
+        ]
         for language in languages:
             yield collections.OrderedDict([
                 ('id', getattr(language, 'alpha_2', language.alpha_3)),
