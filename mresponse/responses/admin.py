a3706837bc4a6dead050cf0b34843152aa96e75d
John Giannelos <jgiannelos@mozilla.com>
2018-11-23 17:40:43 +0100

    Add filters for approval criteria

--- mresponse/responses/admin.py
+++ mresponse/responses/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count, Q
 
 from mresponse.moderations import models as moderations_models
 from mresponse.responses import models as responses_models
@@ -18,6 +19,94 @@ def staff_approve_responses(modeladmin, request, qs):
 staff_approve_responses.short_description = 'Approve responses (staff)'
 
 
+class ModerationsCountFilter(admin.SimpleListFilter):
+    title = 'Moderations'
+    parameter_name = 'moderations'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Yes'),
+            ('no', 'No')
+        )
+
+    def queryset(self, request, queryset):
+        qs = queryset.annotate(total_moderations_count=Count('moderations'))
+        if self.value() == 'yes':
+            qs = qs.filter(total_moderations_count__gte=3)
+        if self.value() == 'no':
+            qs = qs.filter(total_moderations_count__lt=3)
+        return qs.distinct()
+
+
+class PositiveToneCountFilter(admin.SimpleListFilter):
+    title = 'Positive tone'
+    parameter_name = 'positive_tone'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Yes'),
+            ('no', 'No')
+        )
+
+    def queryset(self, request, queryset):
+        qs = queryset.annotate(
+            positive_in_tone_count=Count('moderations', filter=Q(
+                moderations__positive_in_tone=True)
+            ))
+
+        if self.value() == 'yes':
+            qs = qs.filter(positive_in_tone_count__gte=3)
+        if self.value() == 'no':
+            qs = qs.filter(positive_in_tone_count__lt=3)
+        return qs.distinct()
+
+
+class AddressingIssueCountFilter(admin.SimpleListFilter):
+    title = 'Addressing issue'
+    parameter_name = 'addressing_issue'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Yes'),
+            ('no', 'No')
+        )
+
+    def queryset(self, request, queryset):
+        qs = queryset.annotate(
+            addressing_the_issue_count=Count('moderations', filter=Q(
+                moderations__addressing_the_issue=True))
+        )
+
+        if self.value() == 'yes':
+            qs = qs.filter(addressing_the_issue_count__gte=2)
+        if self.value() == 'no':
+            qs = qs.filter(addressing_the_issue_count__lt=2)
+        return qs.distinct()
+
+
+class PersonalCountFilter(admin.SimpleListFilter):
+    title = 'Personalized'
+    parameter_name = 'personal_count'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Yes'),
+            ('no', 'No')
+        )
+
+    def queryset(self, request, queryset):
+        qs = queryset.annotate(
+            personal_count=Count('moderations', filter=Q(
+                moderations__personal=True))
+        )
+
+        if self.value() == 'yes':
+            qs = qs.filter(personal_count__gte=1)
+        if self.value() == 'no':
+            qs = qs.filter(personal_count__lt=1)
+        return qs.distinct()
+
+
 @admin.register(responses_models.Response)
 class ResponseAdmin(admin.ModelAdmin):
     inlines = (ModerationInline,)
@@ -29,6 +118,10 @@ class ResponseAdmin(admin.ModelAdmin):
         'approved',
         'staff_approved',
         'submitted_to_play_store',
+    )
+    list_filter = (
+        ModerationsCountFilter, PositiveToneCountFilter,
+        AddressingIssueCountFilter, PersonalCountFilter
     )
     actions = [staff_approve_responses]
 
