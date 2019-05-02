ddcf71bd7bee5d08bdd2d8d37471f6b49d1bb03e
Rich Brennan <rich.brennan@torchbox.com>
2019-05-02 17:17:16 +0100

    convert user tests to use factories

--- mresponse/users/tests.py
+++ mresponse/users/tests.py
@@ -1,3 +1,6 @@
+import factory
+
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.test import TestCase
@@ -5,19 +8,39 @@ from django.test import TestCase
 User = get_user_model()
 
 
-def get_can_skip_community_response_moderation_user():
-    user = User.objects.create()
-    permission = Permission.objects.get(codename='can_bypass_community_moderation')
-    user.user_permissions.add(permission)
-    return user
+class UserFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = User
+        django_get_or_create = ('username',)
+
+    username = 'jane'
+
+
+class BypassCommunityModerationUserFactory(UserFactory):
+
+    @factory.post_generation
+    def user_permissions(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of permissions were passed in, use them
+            for permission in extracted:
+                self.user_permissions.add(permission)
+
+        self.user_permissions.add(
+            Permission.objects.get(codename='can_bypass_community_moderation')
+        )
 
 
 class TestUsers(TestCase):
 
     def test_can_skip_community_response_moderation(self):
-        user = get_can_skip_community_response_moderation_user()
+        user = BypassCommunityModerationUserFactory()
         self.assertTrue(user.profile.can_skip_community_response_moderation)
 
     def test_cannot_skip_community_response_moderation(self):
-        user = User.objects.create()
+        user = UserFactory()
         self.assertFalse(user.profile.can_skip_community_response_moderation)
