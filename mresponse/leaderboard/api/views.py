9b6ba412116857d037e38140424b64ef2cdf9291
Tomasz Knapik <tomasz.knapik@torchbox.com>
2019-06-06 16:24:33 +0100

    Cache leaderboard for an hour

--- mresponse/leaderboard/api/views.py
+++ mresponse/leaderboard/api/views.py
@@ -1,9 +1,14 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
 from rest_framework import generics
 
 from mresponse.leaderboard.api.serializers import LeaderboardSerializer
 from mresponse.leaderboard.models import Leaderboard
 
 
+# Cache leaderboard for an hour
+@method_decorator(cache_page(60 * 60 * 1), name='retrieve')
 class LeaderboardView(generics.RetrieveAPIView):
     serializer_class = LeaderboardSerializer
     queryset = Leaderboard.objects.all()
