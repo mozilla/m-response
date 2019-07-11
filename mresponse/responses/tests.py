13d1dbfa3658d132b510b1bccc274ced2bd6ee04
Rich Brennan <rich.brennan@torchbox.com>
2019-07-11 15:31:59 +0100

    remove unused imports

--- mresponse/responses/tests.py
+++ mresponse/responses/tests.py
@@ -1,15 +1,16 @@
 import datetime
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission
 from django.test import TestCase
 
 import factory
 from mresponse.applications.models import Application
 from mresponse.reviews.models import Review
-from mresponse.users.tests import (BypassCommunityModerationUserFactory,
-                                   BypassStaffModerationUserFactory,
-                                   UserFactory)
+from mresponse.users.tests import (
+    BypassCommunityModerationUserFactory,
+    BypassStaffModerationUserFactory,
+    UserFactory,
+)
 
 from .api.serializers import ResponseSerializer
 
@@ -17,17 +18,15 @@ User = get_user_model()
 
 
 class ApplicationFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = Application
-        django_get_or_create = ('package',)
+        django_get_or_create = ("package",)
 
-    name = 'Firefox'
-    package = 'org.test.firefox'
+    name = "Firefox"
+    package = "org.test.firefox"
 
 
 class ReviewFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = Review
 
@@ -38,27 +37,22 @@ class ReviewFactory(factory.django.DjangoModelFactory):
 
 
 class ResponseSerializerFactory(factory.Factory):
-
     class Meta:
         model = ResponseSerializer
-        exclude = ('author',)
+        exclude = ("author",)
 
-    data = {'text': 'test'}
+    data = {"text": "test"}
     author = UserFactory
 
     @classmethod
     def create(cls, **kwargs):
         obj = super().create(**kwargs)
         obj.is_valid()
-        obj.save(
-            review=ReviewFactory(),
-            author=kwargs.get('author', UserFactory())
-        )
+        obj.save(review=ReviewFactory(), author=kwargs.get("author", UserFactory()))
         return obj
 
 
 class TestResponseSerializer(TestCase):
-
     def test_cannot_bypass_community_moderation(self):
         serializer = ResponseSerializerFactory()
         self.assertFalse(serializer.instance.approved)
--- mresponse/users/tests.py
+++ mresponse/users/tests.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
-from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
 import factory
@@ -9,36 +8,34 @@ User = get_user_model()
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = User
-        django_get_or_create = ('username',)
+        django_get_or_create = ("username",)
 
-    username = 'jane'
+    username = "jane"
 
 
 class BypassCommunityModerationUserFactory(UserFactory):
-    username = 'andrea'
+    username = "andrea"
 
     @factory.post_generation
     def user_permissions(self, create, extracted, **kwargs):
         self.user_permissions.add(
-            Permission.objects.get(codename='can_bypass_community_moderation')
+            Permission.objects.get(codename="can_bypass_community_moderation")
         )
 
 
 class BypassStaffModerationUserFactory(UserFactory):
-    username = 'sarah'
+    username = "sarah"
 
     @factory.post_generation
     def user_permissions(self, create, extracted, **kwargs):
         self.user_permissions.add(
-            Permission.objects.get(codename='can_bypass_staff_moderation')
+            Permission.objects.get(codename="can_bypass_staff_moderation")
         )
 
 
 class TestUsers(TestCase):
-
     def test_can_skip_community_response_moderation(self):
         user = BypassCommunityModerationUserFactory()
         self.assertTrue(user.profile.can_skip_community_response_moderation)
