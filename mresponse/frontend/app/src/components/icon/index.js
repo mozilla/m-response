d940f5b36338b671880e6cc9f52eade4dfbdff91
merb <michael.erb@erbwebdev.com>
2019-08-01 20:12:40 -0700

    Add copy icon to library

--- mresponse/frontend/app/src/components/icon/index.js
+++ mresponse/frontend/app/src/components/icon/index.js
@@ -29,6 +29,14 @@ export default class Icon extends React.Component {
           </svg>
         )
         break
+      case 'copy':
+        icon = (
+          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+            <rect x="9" y="9" width="13" height="13" rx="2" ry="2"/>
+            <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/>
+          </svg>
+        )
+        break
       default:
         break
     }
