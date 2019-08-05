7d302b7910abb0a79a41cd3a00316f75b7a13fcc
merb <michael.erb@erbwebdev.com>
2019-08-05 11:12:24 -0700

    Keep progress-bar to be unique style name

--- mresponse/frontend/app/src/components/progress-table/index.js
+++ mresponse/frontend/app/src/components/progress-table/index.js
@@ -4,14 +4,14 @@ import './progress-table.scss'
 function ProgressBar ({ width = 0, title, value, maxValue }) {
   if (value != null && maxValue != null) width = (value / maxValue) * 100
   return (
-    <tr className='progress-bar'>
-      <td className='progress-bar-title'>{title}</td>
-      <td className='progress-bar-wrap'>
-        <div className='progress-bar-wrap-level'>
-          <div className='progress-bar-wrap-level-indicator' style={{ width: `${width}%` }} />
+    <tr className='progress-bar-row'>
+      <td className='progress-bar-row-title'>{title}</td>
+      <td className='progress-bar-row-wrap'>
+        <div className='progress-bar-row-wrap-level'>
+          <div className='progress-bar-row-wrap-level-indicator' style={{ width: `${width}%` }} />
         </div>
       </td>
-      <td className='progress-bar-label'>{value} / {maxValue}</td>
+      <td className='progress-bar-row-label'>{value} / {maxValue}</td>
     </tr>
   )
 }
